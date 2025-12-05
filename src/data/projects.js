40ec5ec004ac35355b02bf76d5b5e7b707b511ba
Jithu-SM <jithusmusic@gmail.com>
2025-12-05 10:35:06 +0530

    updated projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,17 @@
 export default [
   {
+    title: "AI-Based Employee Recruitment System",
+    description: "Resume parser + Job matching using ML, Django, PostgreSQL.",
+    tech: ["Django", "React", "PostgreSQL", "spaCy", "scikit-learn"],
+    link: "https://github.com/Jithu-SM/employee-recruitment-system",
+  },
+  {
+    title: "Online Music Streaning Platform",
+    description: "Web based Music Streaming Platform with user authentication and playlist management",
+    tech: ["PHP", "MySQL", "Bootstrap", "HTML", "JavaScript"],
+    link: "https://github.com/Jithu-SM/online-music-streaming-platform-php",
+  },
+  {
     title: "Hidden File Vault App",
     description: "Android app with secret dialer code, biometric lock, and file encryption.",
     tech: ["Java", "Android Studio"],
@@ -10,11 +22,5 @@ export default [
     description: "Custom lexer, tokens, REPL, and upcoming interpreter.",
     tech: ["Python", "Compiler Design"],
     link: "https://github.com/jithu-sm/SyntaxError-lang",
-  },
-  {
-    title: "AI-Based Employee Recruitment System",
-    description: "Resume parser + Job matching using ML, Django, PostgreSQL.",
-    tech: ["Django", "React", "ML", "PostgreSQL"],
-    link: "#",
   },
 ];
